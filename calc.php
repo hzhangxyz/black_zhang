b7d88a5ab021b03b39a055742f31d1006fdcf2c7
trumpet <zh19970205@126.com>
2016-02-25 00:09:23 +0800

    Update calc.php

--- calc.php
+++ calc.php
@@ -1,16 +1,39 @@
 <?php
 function gate($what,$who){
+    $con = mysql_connect("localhost","root","");
+    mysql_select_db("mysql", $con);
+    $result = mysql_query('SELECT lang FROM lang WHERE name="$who"');
+    if($row = mysql_fetch_array($result)){
+        switch($row["lang"]){
+            case "php":
+                $ans = run_php($what);
+                break;
+            case "python":
+                $ans = $ans;
+                break;
+            default:
+                mysql_query("UPDATE lang SET lang="php" WHERE name="$who"');
+                $ans = run_php($what);
+        }
+    }else{
+        mysql_query('INSERT INTO lang (name, lang) VALUES ("$who", "php")');
+        $ans = run_php($what);
+    }
+    mysql_close($con);
+    return $ans;
+}
+
+function run_php($what){
+    $ans = $what;
     try{
-        $ans = $what;
         eval($ans);
-        $what =$ans;
     }
     catch(Exception $e){}
     try{
-        eval('$what='.$what.';');
+        eval('$ans='.$ans.';');
     }
     catch(Exception $e){}
-    return $what;
+    return $ans;
 }
 
 ?>
