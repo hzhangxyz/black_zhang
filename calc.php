217a94b7a4f9125b7aa8956ce88cf0d7255fac6b
trumpet <zh19970205@126.com>
2016-02-25 00:39:40 +0800

    Update calc.php

--- calc.php
+++ calc.php
@@ -3,9 +3,9 @@ function runer($cmd,$script){
     $descriptorspec = array(
         0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
         1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
-        2 => array("file", "/tmp/error-output.txt", "a") // stderr is a file to write to
+        2 => array("file", "/dev/null", "a") // stderr is a file to write to
     );
-    $cwd = '/tmp';
+    $cwd = '/';
     $env = array('some_option' => 'aeiou');
     $process = proc_open($cmd, $descriptorspec, $pipes, $cwd, $env);
     fwrite($pipes[0], $script);
