2226919a8f1bcf66f65e57a5f385f357f6a64100
trumpet <zh19970205@126.com>
2016-02-29 07:41:48 +0000

    sql init

--- init.sql
+++ init.sql
@@ -1,3 +1,3 @@
 USE mysql;
-DROP TABLE lang;
-CREATE TABLE lang(name CHAR(32), lang CHAR(32), if_cache CHAR(1), cache CHAR(1024));
+DROP TABLE IF EXISTS lang ;
+CREATE TABLE lang(name CHAR(32), lang CHAR(32), if_cache CHAR(1), cache CHAR(255));
