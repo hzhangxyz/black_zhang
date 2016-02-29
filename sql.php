2078e8d2bc46aa4062604bfb92c62d8cd88cb243
trumpet <zh19970205@126.com>
2016-02-29 08:04:55 +0000

    update

--- sql.php
+++ sql.php
@@ -2,8 +2,9 @@
 
 function set($name, $arr){
     $con = mysql_connect("localhost","root","");
+    mysql_select_db("mysql",$con);
     $result = mysql_query('SELECT * FROM lang WHERE name="'.$name.'"');
-    if(!($row = mysql_fetch_array($result))){
+    if(!(mysql_fetch_array($result))){
         mysql_query('INSERT INTO lang (name, lang, if_cache, cache) VALUE ("'.$name.'", "normal", "N","")');
     }
     if($arr["lang"])mysql_query('UPDATE lang SET lang="'.$arr["lang"].'" WHERE name="'.$name.'"');
